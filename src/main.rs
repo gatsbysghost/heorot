119e1bba86d29da760d19e0c0c08c7d4e14057ae
Scott Reu <sreu@meraki.com>
2021-09-10 09:49:51 -0700

    Replace main with public, unmangled _start func

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
+/// This gets called in the event of a panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn main() {}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
