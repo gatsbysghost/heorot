974bc80b0fa2af6e2786778bedf5d8731f575dc7
Scott Reu <sreu@meraki.com>
2021-09-10 11:54:38 -0700

    Print panic message when we panic with println

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello world{}", "!");
-
     loop {}
 }
 
 /// This gets called in the event of a panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
