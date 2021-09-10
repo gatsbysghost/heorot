ffb69bc6ba44d09f963fd27d5180c6c5a25a54da
Scott Reu <sreu@meraki.com>
2021-09-10 11:42:00 -0700

    Make WRITER a Mutex-locked resource

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,6 +1,7 @@
 use volatile::Volatile;
 use core::fmt;
 use lazy_static::lazy_static;
+use spin::Mutex;
 
 /// Static values for colors in a C-style struct
 #[allow(dead_code)] // Don't throw compiler errors for unused items
@@ -127,24 +128,9 @@ impl fmt::Write for Writer {
 }
 
 lazy_static! {
-    pub static ref WRITER: Writer = Writer {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Green, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-}
-
-/// Test function to print a Hello World! (remove later)
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("World!");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+    });
 }
