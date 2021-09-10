52a5f060b38b4b18db79430e60accc25a7480094
Scott Reu <sreu@meraki.com>
2021-09-10 15:56:49 -0700

    Simplify one-off testing with harness option

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,18 +1,20 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use heorot::{QemuExitCode, exit_qemu, serial_println};
-use heorot::serial_print;
+use heorot::{exit_qemu, serial_print, serial_println, QemuExitCode};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    loop{}
+}
 
-    loop {}
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
 }
 
 #[panic_handler]
@@ -20,21 +22,4 @@ fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
     loop {}
-}
-
-/// We want to use this instead of the one from src/lib.rs because in this case Panic isn't our failure case, we want it to happen.
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn should_fail() {
-    serial_print!("should_panic::should_fail...\t");
-    assert_eq!(0, 1);
 }
