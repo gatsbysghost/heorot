93f6073129b06ef404c9e41b2ff4ddc226c43c1e
Scott Reu <sreu@meraki.com>
2021-09-18 12:13:58 -0700

    Merge in changes from last commit and some initial page fault work

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,5 +1,7 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::PageFaultErrorCode;
 use pic8259::ChainedPics;
+use crate::hlt_loop;
 use crate::println;
 use crate::print;
 use crate::gdt;
@@ -40,6 +42,8 @@ lazy_static! {
             .set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+
+        idt.page_fault.set_handler_fn(page_fault_handler);
 
         idt
     };
@@ -106,6 +110,19 @@ extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame, _error_code: u64) -> !
 {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 /// TESTS
--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ pub extern "C" fn _start() -> ! {
 
     heorot::init();
 
+    let ptr = 0xdeadbeaf as *mut u32;
+    unsafe { *ptr = 42; }
+
     #[cfg(test)]
     test_main();
 
